3acdb066f3f955c9454a7a9c0be70903c2d2e470
Emmanuel Utomi <eutomi2@uic.edu>
2019-05-05 16:51:04 -0500

    Add integrity check scripts

--- run.sh
+++ run.sh
@@ -5,5 +5,8 @@
 #
 #  Created by Emmanuel Utomi on 5/5/19.
 #  Copyright © 2019 Uche. All rights reserved.
+cd proj6
 make all
-./derby
+./derby > results.txt
+
+cat results.txt | awk '$1=="Horse:" {print $2"\t"$4"\t"$6"\t"$8}' | sed 's\,\\g' | ./clash_detection.py
